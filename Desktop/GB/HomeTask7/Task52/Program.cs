cc5907a024139c1f782bfe42091eb76aadb391cc
AlexVi <vpa24082012@gmal.com>
2023-06-11 21:41:21 +0300

    Добавили решение задачи 52

--- Desktop/GB/HomeTask7/Task52/Program.cs
+++ Desktop/GB/HomeTask7/Task52/Program.cs
@@ -5,3 +5,134 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("введите количество строк");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{                     //   0      1    
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // rows = arr.GetLength(0)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // columns = arr.GetLength(1)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write($"|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("| ");
+    }
+}
+
+Console.WriteLine("Введите координаты");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+int [,] numbers = new int [10,10];
+{
+    if (a > numbers.GetLength(0) || b > numbers.GetLength(1))
+    {
+        Console.WriteLine("такого элемента нет");
+    }
+    else
+    {
+        Console.WriteLine($"значение элемента {a} строки и {b} столбца равно {numbers[a - 1, b - 1]}");
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, -10, 10);
+PrintMatrix(array2d);
+
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine("введите количество строк");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] numbers = new int[n, m];
+FillArrayRandomNumbers(numbers);
+
+
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        avarage = (avarage + numbers[i, j]);
+    }
+    avarage = avarage / n;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
+PrintArray(numbers);
+
+
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+Console.WriteLine("Введите координаты");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+int SrArifmColumns = new int [10,10];
+{
+    if (a > numbers.GetLength(0) || b > numbers.GetLength(1))
+    {
+        Console.WriteLine("такого элемента нет");
+    }
+    else
+    {
+        Console.WriteLine($"значение элемента {a} строки и {b} столбца равно {numbers[a - 1, b - 1]}");
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(3, 4, -10, 10);
+PrintMatrix(array2d);
